1a379c8edaf94d888d5e7f340e187c688299cc43
Ricardo Tavares <ricardotavares51@hotmail.com>
2019-11-01 17:42:18 -0300

    Versão 0.10.8
    Ajustes de Design

--- lib/objects/serverClass.dart
+++ lib/objects/serverClass.dart
@@ -14,8 +14,8 @@ class Server{
     return <Server>[
       Server(1,'Wikipédia', '185.201.54.47', 'https://www.wikipedia.org/', 'assets/wikipedia.svg'),
       Server(2,'Google', '216.58.222.68', 'https://www.google.com/', 'assets/google.svg'),
-      Server(3,'Twitter', '104.244.42.65', 'https://twitter.com/', 'assets/google.svg'),
-      Server(4,'Facebook', '157.240.12.35', 'https://www.facebook.com/', 'assets/google.svg'),
+     // Server(3,'Twitter', '104.244.42.65', 'https://twitter.com/', 'assets/google.svg'),
+      Server(3,'Facebook', '157.240.12.35', 'https://www.facebook.com/', 'assets/facebook.svg'),
     ];
   }
 }
